fa65fdeaf19f9b8ca51a92a0225578ea49d6c8b9
SariNeverfinal <sari@neverfinal.com>
2025-10-26 17:08:20 +0100

    add favico

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -21,6 +21,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Social Spots",
   description: "Discover and Share the Best Social Spots Around You",
+  icons: {
+    icon: "/favicon.ico"
+  }
 };
 
 export default async function RootLayout({children}: Readonly<{children: React.ReactNode}>) {
